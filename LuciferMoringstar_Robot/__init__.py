2990ff832449d0f754096bc408242f867de62885
BROOK980 <104886250+BROOK980@users.noreply.github.com>
2022-05-09 10:42:45 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -28,14 +28,14 @@ Bot Owner Only
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
+➥ Name :  Sunny Wayne
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [KL CINEMA CHAKKU GROUP 🎬🎬](
+https://t.me/MM_CINEMA_CLUB_GROUP)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ Tutorial : [Click Me](https://t.me/MM_CINEMA_CLUB_GROUP)
 """
